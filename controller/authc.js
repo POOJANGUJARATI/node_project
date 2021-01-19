09d1bf6d5bcf8fa05026687034fefe14ff76404c
poojan <pojangujarati20@gmail.com>
2021-01-19 13:14:08 +0530

    \initial commit

--- controller/authc.js
+++ controller/authc.js
@@ -3,34 +3,56 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { Mongoose } = require('mongoose')
 
+function ValidateEmail(mail) {
+     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+          return true
+     }
+     return false
+}
+
 const register = (req, res) => {
-     if ((req.body.password).length >= 8 && (req.body.password).length <= 25){
-          // console.log("ss")
-     bcrypt.hash(req.body.password, 10, function (err, hashedpass) {
-          if (err) {
+     if ((req.body.password).length >= 8 && (req.body.password).length <= 25) {
+          if ((req.body.phone.length == 10)) {
+               if (ValidateEmail(req.body.email)) {
+
+                    // console.log("ss")
+                    bcrypt.hash(req.body.password, 10, function (err, hashedpass) {
+                         if (err) {
+                              res.json({
+                                   error: err
+                              })
+                         }
+                         let user = new User({
+                              name: req.body.name,
+                              email: req.body.email,
+                              password: hashedpass,
+                              phone: req.body.phone
+                         })
+                         user.save().then(user => {
+                              res.json({
+                                   message: 'user added succesfully'
+                              })
+                         }).catch(error => {
+                              res.json({
+                                   message: 'Aww.. Somthing Goes Wrong'
+                              })
+                         })
+                    })
+               } 
+               else {
+                    res.json({
+                         message: "invalid email address"
+                    })
+               }
+          } 
+          else {
                res.json({
-                    error: err
+                    message: "Invalid Phone Number"
                })
           }
-          let user = new User({
-               name: req.body.name,
-               email: req.body.email,
-               password: hashedpass,
-               phone: req.body.phone
-          })
-          user.save().then(user => {
-               res.json({
-                    message: 'user added succesfully'
-               })
-          }).catch(error => {
-               res.json({
-                    message: 'Aww.. Somthing Goes Wrong'
-               })
-          })
-     })
-     }else{
+     } else {
           res.json({
-               message : "password length does not validate"
+               message: "password length does not validate"
           })
      }
 }
